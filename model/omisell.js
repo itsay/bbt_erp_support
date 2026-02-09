93b7c8bbab4009e90a386725d5eb3882ebe6a12e
Hieu.ND <ndhieuvegia983@gmail.com>
2026-02-09 13:06:04 +0700

    impl: save webhook tracking

--- model/omisell.js
+++ model/omisell.js
@@ -21,10 +21,14 @@ const OrderRevenue = mongoose.Schema({
 const PickupList = mongoose.Schema({
 }, { versionKey: false, timestamps: false, strict: false })
 
+const WebhookEvent = mongoose.Schema({
+}, { versionKey: false, timestamps: false, strict: false })
+
 
 module.exports = {
   Order: mongoose.model('bbt_omisell_orders', Order),
   OrderDetail: mongoose.model('bbt_omisell_order_detail', OrderDetail),
   OrderRevenue: mongoose.model('bbt_omisell_order_revenue', OrderRevenue),
-  PickupList: mongoose.model('bbt_omisell_pickup', PickupList)
+  PickupList: mongoose.model('bbt_omisell_pickup', PickupList),
+  WebhookEvent: mongoose.model('bbt_omisell_webhook_event', WebhookEvent)
 }
--- route/webhook/webhook.controller.js
+++ route/webhook/webhook.controller.js
@@ -1,7 +1,17 @@
+const { WebhookEvent } = require('../../model/omisell')
+const StatusWebhookEnum = require('../../common/enums/status-webhook.eum')
+
 function WebhookController() {
     return {
         receiveWebhook: async (req, res) => {
             console.log(`[receiveWebhook] - payload: ${JSON.stringify(req.body)}`);
+            WebhookEvent.create({
+                ...req.body,
+                receivedAt: new Date(),
+                status: StatusWebhookEnum.PENDING,
+            }).catch((err) => {
+                console.log(`[receiveWebhook] - saveTracking - fail: `, err.stack);
+            })
             return res.json({ msg: "Webhook received" });
         }
     }
