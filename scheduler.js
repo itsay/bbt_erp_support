06c5fbbe0ab50413f2f930c46b16f94899cfedf7
Khoa Nguyen Dang <ndkhoa0704@gmail.com>
2026-02-24 10:51:42 +0700

    feat: initialize Redis connection in scheduler

--- scheduler.js
+++ scheduler.js
@@ -1,6 +1,8 @@
 require("dotenv").config({ path: __dirname + "/.env" });
 const SchedulerService = require("./service/scheduler.service");
 const mongoose = require("mongoose");
+const RedisService = require("./route/util/redisController");
+RedisService.initConnection();
 
 mongoose.set("strictQuery", true);
 mongoose.Promise = global.Promise;
